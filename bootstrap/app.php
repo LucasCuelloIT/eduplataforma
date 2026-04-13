087c0aa8db7d1de18badace500e66dda7668c822
LucasCuelloIT <lukio_o@hotmail.com>
2026-04-13 12:09:37 +0000

    Middleware ForceHttps para todos los formularios

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
+        $middleware->prepend(\App\Http\Middleware\ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
